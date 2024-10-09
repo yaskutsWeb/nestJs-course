32f8d1808710e7620da92a8c270a244c689f902f
pavel.yaskuts <pavel.yaskuts@expert-soft.by>
2024-10-09 21:34:01 +0300

    module 5

---
+++ source/module 5/3. Setting Up DI Between Modules/di/src/cpu/cpu.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
+
+@Injectable()
+export class CpuService {
+  constructor(private powerService: PowerService) {}
+}
