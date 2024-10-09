937e6505a097f38e17256f48f760098052273fff
pavel.yaskuts <pavel.yaskuts@expert-soft.by>
2024-10-09 19:10:56 +0300

    module 4

---
+++ source/module 4/5. Manual Testing of the Controller/messages/src/messages/messages.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DON'T DO THIS ON REAL APP (USE DI)
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
+}
