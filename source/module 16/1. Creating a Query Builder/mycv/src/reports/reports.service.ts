e35f2ee5200fbd80e775f0d7300a5bb297a6b195
pavel.yaskuts <pavel.yaskuts@expert-soft.by>
2024-10-11 01:47:28 +0300

    module 16

---
+++ source/module 16/1. Creating a Query Builder/mycv/src/reports/reports.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
+
+@Injectable()
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createEstimate(estimateDto: GetEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('*')
+      .where('make = :make', { make: estimateDto.make })
+      .getRawMany();
+  }
+
+  create(reportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(reportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
+    return this.repo.save(report);
+  }
+}
