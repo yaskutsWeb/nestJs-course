5a42d35fa7254b2067968746ce91181763069845
pavel.yaskuts <pavel.yaskuts@expert-soft.by>
2024-10-11 00:25:00 +0300

    module 14

---
+++ source/module 14/11. Setting up the Association/mycv/src/reports/reports.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+
+@Controller('reports')
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+}
---
+++ source/module 14/3. A DTO for Report Creation/mycv/src/reports/reports.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Post, Body } from '@nestjs/common';
+
+@Controller('reports')
+export class ReportsController {
+  @Post()
+  createReport(@Body() body: CreateReportDto) {}
+}
